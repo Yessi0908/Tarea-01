201e5cd43be33527769ccf1f75c7ca2d777a1b3d
Yessi0908 <106893050+Yessi0908@users.noreply.github.com>
2023-03-04 00:01:11 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics.Eventing.Reader;
+
+namespace condiciones
+{
+    class SentenciasIf
+    {
+        public static void Main(string[] args)
+        {
+            int numeroentero = int.Parse(Console.ReadLine());
+            if (numeroentero == 7)
+            Console.WriteLine("numeroentero es 7. ");
+
+            else if (numeroentero < 0)
+                Console.WriteLine("numeroentero es negativo");
+            else
+                Console.WriteLine("numeroentero no es ninguno de los casos programados");
+        }
+        }
+    }
+}
---
+++ Program4.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Entrada_salida
+{
+    class Entradasalida
+    {
+       public static void Main(string[] args)
+        {
+            string nombre;
+
+            Console.WriteLine("Escribe tu nombre:  ");
+            nombre= Console.ReadLine();
+            Console.WriteLine(" hola "+ nombre);
+            Console.ReadLine();
+        }
+    }
+}
